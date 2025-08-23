d659dccbe961e449909d071dc4611c8009bf396f
Harrison Lee <harrisonrexlee@gmail.com>
2025-08-23 18:06:57 -0400

    States what should be included in coinflip.py(for now)

---
+++ coinflip/objective in coinflip.py
@@ -0,0 +1,11 @@
+# A list of objectives for the coin flip simulator project
+#1. Build a coin flip simulator
+#2. Add probability calculations
+#3. Make the program repeatable with user input by game loop style
+#4. Build ineractiveness for non GUI
+#5. Add a GUI
+#6. Add interactivity to the GUI
+#7. Get rid of game loop style and replace with CLI style
+#8. Add statistics
+#9.Optimize code
+#10. Fix bugs
