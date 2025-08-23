66a320ba23d2af4496467972b2c4a4802976c6af
Harrison Lee <harrisonrexlee@gmail.com>
2025-08-23 18:22:24 -0400

    Sates what should be included in dicerolls.py

---
+++ dicerolls/objective in dicerolls.py
@@ -0,0 +1,8 @@
+# A list for the objectives of the dice rolls simulator project
+#1. Build a dice roll simulator
+#2. Add probability calculations
+#3. Make the program repeatable with user input by game loop style
+#4. Add statistics
+#5. Optimize code
+#6. Fix bugs
+# No GUI yet
